295689fc26d9952cb91af1b8f78d9236cabdd5fb
Heather Hartley <hlhartley@gmail.com>
2019-05-09 16:02:35 -0600

    Adds styling

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $('.republican').addClass('red');
 $('.democrat').addClass('blue');
 $('.term').addClass('yellow');
-$('.whig').css({ backgroundColor: 'purple', color: 'white'});
+$('.whig').css({ backgroundColor: '#673ab7', color: 'white'});
 
 $('.died .term').append('<span class="died"> (Died)</span>')
