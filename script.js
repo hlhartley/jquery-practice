34b0416a554393a138131a68b1610e0a4a246e51
Heather Hartley <hlhartley@gmail.com>
2019-05-09 15:44:11 -0600

    Adds styles.css file and adds jquery in script.js file

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-$('tr').css({ backgroundColor: 'blue'})
+$('.republican').addClass('red');
+$('.democrat').addClass('blue');
+$('.term').addClass('yellow');
+$('.whig').css({ backgroundColor: 'purple', color: 'white'});
+
+$('.died .term').append('<span class="died"> (Died)</span>')
